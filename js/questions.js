4e32eb203a558b07a48f6a9b00834eeb1a161a5c
zmar0519 <zachary.p.martin7@gmail.com>
2021-06-25 13:29:26 -0500

    complete coding and movie trivia

---
+++ js/questions.js
@@ -0,0 +1,29 @@
+function getRandomMovieQuestion() {
+    return movieTrivia[Math.floor(Math.random() * (movieTrivia.length - 1))]
+  }
+
+const movieTrivia = [
+    'In the matrix does Neo take the blue pill or the red pill?',
+    'Who played Forest Gump in the movie Forest Gump?',
+    'Who did Rose tell she would never let go in Titanic(immediately before letting go)?',
+    "How many suns does Luke's home plannet of Tatoine have?",
+    "What movie has the record for most 'F-Bombs' in a single move?",
+    "What character in Monty Python famously says 'Tis just a flesh wound?'",
+    "What is the name of the movie about the Jamaican Bobsled team?",
+    "Who directed Titanic, Avatar, and Terminator?",
+    "Who is the main antagonist from the Harry Potter series?",
+    "Which Indianna Jone's movie starred Shia Labeouf?"
+]
+
+const codingTrivia = [
+    'What year was the first computer virus created in?',
+    'How many different programming languages are there?',
+    'What is the format that is used for transporting and storing code called?',
+    'What is the code used to perform a specific task called?',
+    'JavaScript is which of the following?',
+    'What type of variable would a line of text be?',
+    'Which syntax for defining a variable is no longer considered best practice?',
+    'Which of the following languages is not a frontend language?',
+    'Which of the following tags denotes a header in HTML',
+    'What is the most popular coding language in the world according to GitHub?'
+]
