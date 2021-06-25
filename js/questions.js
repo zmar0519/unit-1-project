0970f0a66226cef58d93741141161f56e718a173
zmar0519 <zachary.p.martin7@gmail.com>
2021-06-25 15:53:23 -0500

    write history trivia

--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,14 @@
 function getRandomMovieQuestion() {
     return movieTrivia[Math.floor(Math.random() * (movieTrivia.length - 1))]
-  }
+}
+
+function getRandomCodingQuestion() {
+    return codingTrivia[Math.floor(Math.random() * (codingTivia.length - 1))]
+}
+
+function getRandomHistoryQuestion() {
+    return historyTrivia[Math.floor(Math.random() * (historyTrivia.length - 1))]
+}
 
 const movieTrivia = [
     'In the matrix does Neo take the blue pill or the red pill?',
@@ -26,4 +34,17 @@ const codingTrivia = [
     'Which of the following languages is not a frontend language?',
     'Which of the following tags denotes a header in HTML',
     'What is the most popular coding language in the world according to GitHub?'
+]
+
+const historyTrivia = [
+    'What was the first sport played on the Moon?',
+    'How many plays did Shakespear write?',
+    'Who was the first person to circumnavigate the globe?',
+    'How many ammendments are there to the constitution?',
+    'What year did the United States become a country?',
+    'What president is on the $20 bill?',
+    'How many blimps are there in the world?',
+    'During the Cold War what beverage company had the 7th largest Navy in the world?',
+    'In 1932 Australia declared war against what type of bird?',
+    'What was the crown at the Empire State Building originally for?'
 ]
