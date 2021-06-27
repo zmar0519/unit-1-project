209f66b20ae224d16df9099275d7e00852e4f0cf
zmar0519 <zachary.p.martin7@gmail.com>
2021-06-27 07:58:52 -0500

    add event listeners and cached element refs

--- js/app.js
+++ js/app.js
@@ -11,15 +11,23 @@ let previousQuestions = []
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+const movieBtn = document.querySelector('#movie-button')
+const codingBtn = document.querySelector('#coding-button')
+const historyBtn = document.querySelector('#history-button')
+const scienceBtn = document.querySelector('#science-button')
+const randomBtn = document.querySelector('#random-button')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
+movieBtn.addEventListener('click', initMovieQuiz)
+codingBtn.addEventListener('click', initCodingQuiz)
+historyBtn.addEventListener('click', initHistoryQuiz)
+scienceBtn.addEventListener('click', initScienceQuiz)
+randomBtn.addEventListener('click', initRandomQuiz)
 
 /*-------------------------------- Functions --------------------------------*/
 
-import { getRandomKanyeQuote, getRandomTaylorQuote } from "../data/quotes.js"
+import { getRandomMovieQuestion, getRandomCodingQuestion, getRandomHistoryQuestion, getRandomScienceQuestion } from "../Data/questions.js"
 
 function render() {
 
