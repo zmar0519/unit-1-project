bf87cdf60949b0f6a74353ffdd7792bf846ae931
zmar0519 <zachary.p.martin7@gmail.com>
2021-06-28 02:02:06 -0500

    finish function to return Q's

--- js/app.js
+++ js/app.js
@@ -17,26 +17,39 @@ const codingBtn = document.querySelector('#coding-button')
 const historyBtn = document.querySelector('#history-button')
 const scienceBtn = document.querySelector('#science-button')
 const randomBtn = document.querySelector('#random-button')
+const questionHolder = document.querySelector('#question-holder')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 movieBtn.addEventListener('click', initMovieQuiz)
-// codingBtn.addEventListener('click', initCodingQuiz)
-// historyBtn.addEventListener('click', initHistoryQuiz)
-// scienceBtn.addEventListener('click', initScienceQuiz)
-// randomBtn.addEventListener('click', initRandomQuiz)
+codingBtn.addEventListener('click', initCodingQuiz)
+historyBtn.addEventListener('click', initHistoryQuiz)
+scienceBtn.addEventListener('click', initScienceQuiz)
+randomBtn.addEventListener('click', initRandomQuiz)
 
 /*-------------------------------- Functions --------------------------------*/
 
 function initMovieQuiz() {
-questionHolder.innerHTML = "`${currentQuestion}`"
-}
+    questionHolder.innerHTML = `${currentQuestion}`
+    console.log('Movie Q')
+} // This makes the logic for each of the buttons work. Pressing will return a question from Movie Array.
 
+function initCodingQuiz() {
+    questionHolder.innerHTML = `${currentQuestion}`
+    console.log('Coding Q') 
+} // This makes the logic for each of the buttons work. Pressing will return a question from Coding Array.
 
-function initCodingQuiz()
+function initHistoryQuiz(){
+    questionHolder.innerHTML = `${currentQuestion}`
+    console.log('History Q')
+} // This makes the logic for each of the buttons work. Pressing will return a question from History Array.
 
-function initHistoryQuiz()
+function initScienceQuiz(){
+    questionHolder.innerHTML = `${currentQuestion}`
+    console.log('Science Q')
+} // This makes the logic for each of the buttons work. Pressing will return a question from Science Array.
 
-function initScienceQuiz()
-
-function initRandomQuiz()
+function initRandomQuiz() {
+    questionHolder.innerHTML = `${currentQuestion}`
+    console.log('Random Q')
+} // This makes the logic for each of the buttons work. Pressing will return a question from Random Array.
